7569eba6acdfe334e81f54334646b8290a796686
David Montenegro <dnmontenegro@email.wm.edu>
2021-06-12 19:27:01 -0400

    Upload MazeFactoryTest.java
    Initial pseudocode testcases

---
+++ MazeByDavidMontenegro/test/generation/MazeFactoryTest.java
@@ -0,0 +1,50 @@
+package generation;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class MazeFactoryTest {
+	
+	private MazeFactory mazeFactory;
+	private StubOrder stubOrder;
+	private MazeContainer mazeConfig;
+	
+	@Before
+	public void setUp() {
+		// Setup MazeFactory
+		// Setup StubOrder
+		// Order maze from the MazeFactory
+		// Wait until the order is done
+		// Get maze from the StubOrder
+	}
+	
+	@After
+	public void tearDown() throws Exception {
+		// Test exit
+	}
+	
+	@Test
+	public final void testMazeFactory() {
+		// Check if objects are not null
+	}
+	
+	@Test
+	public final void testMazeFactorOrder() {
+		// Check if method returns true with valid input
+		// Check if method returns false when one order is currently being processed
+	}
+	
+	@Test
+	public final void testMazeFactoryCancel() {
+		// Check if objects are set to null
+	}
+	
+	@Test
+	public final void testMazeFactoryWaitTillDelivered() {
+		// Check if objects are set to null
+	}
+	
+	
+	
+}
