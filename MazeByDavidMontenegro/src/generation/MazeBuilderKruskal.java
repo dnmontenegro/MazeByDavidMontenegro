32a272706cb248c33365a3a4f53316b4b28ad9f9
David Montenegro <dnmontenegro@email.wm.edu>
2021-06-13 05:45:59 -0400

    Upload MazeBuilderKruskalTest.java and MazeBuilderKruskal code

--- MazeByDavidMontenegro/src/generation/MazeBuilderKruskal.java
+++ MazeByDavidMontenegro/src/generation/MazeBuilderKruskal.java
@@ -6,6 +6,7 @@ public class MazeBuilderKruskal extends MazeBuilder implements Runnable {
 	
 	public MazeBuilderKruskal() {
 		// Call to super
+		super();
 	}
 
 	@Override
@@ -15,12 +16,38 @@ public class MazeBuilderKruskal extends MazeBuilder implements Runnable {
 		// Go through candidates list randomly removing a wallboard
 		// If neighboring slot is not yet in the path then the wallboard is deleted
 		// The matrix keeps track of the current path
+		final ArrayList<Wallboard> candidates = new ArrayList<Wallboard>();
+		updateListOfWallboards(candidates);
+		
+		int count = 1;
+		int[][] board = new int[width][height];
+		for (int i = 0; i < width; i++) {
+			for (int j = 0; j < height; j++) {
+				board[i][j] = count;
+				count++;
+			}
+		}
+		
+		Wallboard curWallboard;
+		while(!candidates.isEmpty()){
+			curWallboard = extractWallboardFromCandidateSetRandomly(candidates);
+			if (board[curWallboard.getX()][curWallboard.getY()] != board[curWallboard.getNeighborX()][curWallboard.getNeighborY()]) {
+				floorplan.deleteWallboard(curWallboard);
+				int value = board[curWallboard.getX()][curWallboard.getY()];
+				for (int i = 0; i < width; i++) {
+					for (int j = 0; j < height; j++) {
+						if (board[i][j] == value)
+							board[i][j] = board[curWallboard.getNeighborX()][curWallboard.getNeighborY()];
+					}
+				}
+			}
+		}
 	}
 	
 	private Wallboard extractWallboardFromCandidateSetRandomly(final ArrayList<Wallboard> candidates) {
-		// Taken from MazeBuilderPrim class
 		// Remove a random candidate from list of candidates and return it
-		return null;
+		// Taken from MazeBuilderPrim class
+		return candidates.remove(random.nextIntWithinInterval(0, candidates.size()-1)); 
 	}
 	
 	private void updateListOfWallboards(ArrayList<Wallboard> wallboards) {
@@ -28,5 +55,18 @@ public class MazeBuilderKruskal extends MazeBuilder implements Runnable {
 		// Create a new wallboard
 		// Set wallboards to all directions of a slot
 		// Add wallboards that aren't on the maze borders to the candidates list
+		for (int i = 0; i < width; i++) {
+			for (int j = 0; j < height; j++) {
+				// Taken from MazeBuilderPrim class
+				Wallboard wallboard = new Wallboard(i, j, CardinalDirection.East) ;
+				for (CardinalDirection cd : CardinalDirection.values()) {
+					wallboard.setLocationDirection(i, j, cd);
+					if (floorplan.canTearDown(wallboard))
+					{
+						wallboards.add(new Wallboard(i, j, cd));
+					}
+				}
+			}
+		}
 	}
 }
---
+++ MazeByDavidMontenegro/test/generation/MazeBuilderKruskalTest.java
@@ -0,0 +1,162 @@
+package generation;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class MazeBuilderKruskalTest extends MazeFactoryTest {
+
+	private MazeFactory mazeFactory;
+	private StubOrder stubOrder;
+	private StubOrder stubOrder2;
+	private StubOrder stubOrder3;
+	private MazeContainer mazeConfig;
+	private MazeContainer mazeConfig2;
+	
+	@Before
+	public void setUp() {
+		// Setup MazeFactory
+		// Setup StubOrder
+		mazeFactory = new MazeFactory();
+		stubOrder = new StubOrder(13, 0, Order.Builder.Kruskal, true);
+		stubOrder2 = new StubOrder(13, 1, Order.Builder.Kruskal, false);
+		stubOrder3 = new StubOrder(13, 0, Order.Builder.Eller, true);
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		// Test exit
+	}
+	
+	@Test
+	public void testMazeFactory() {
+		// Check if objects are not null
+		assertNotNull(mazeFactory);
+		assertNotNull(stubOrder);
+		assertNotNull(stubOrder2);
+	}
+	
+	@Test
+	public void testMazeFactoryOrder() {
+		// Check if method returns true with DFS input
+		// Check if method returns false when one order is currently being processed
+		// Check if method returns true with Prim input
+		// Check if method returns false when algorithm is not covered
+		assertTrue(mazeFactory.order(stubOrder));
+		mazeFactory.waitTillDelivered();
+		mazeFactory.order(stubOrder);
+		assertFalse(mazeFactory.order(stubOrder2));
+		mazeFactory.waitTillDelivered();
+		assertTrue(mazeFactory.order(stubOrder2));
+		mazeFactory.waitTillDelivered();
+		assertFalse(mazeFactory.order(stubOrder3));
+	}
+	
+	@Test
+	public void testMazeFactoryCancel() {
+		// Check if calling method with no thread running does not interrupt future operations
+		// Check if calling method works when an order is being processed
+		mazeFactory.cancel();
+		assertTrue(mazeFactory.order(stubOrder));
+		mazeFactory.waitTillDelivered();
+		mazeFactory.order(stubOrder2);
+		mazeFactory.cancel();
+		
+	}
+	
+	@Test
+	public void testMazeFactoryWaitTillDelivered() {
+		// Check if calling method with no thread running does not interrupt future operations
+		mazeFactory.waitTillDelivered();
+		assertTrue(mazeFactory.order(stubOrder));
+	}
+	
+	
+	@Test
+	public void testMazeOneExit() {
+		// Order maze from the MazeFactory
+		// Wait until the order is done
+		// Get maze from the StubOrder
+		// Keep track of number of exits
+		// Get distance from exit object of maze
+		// Loop through with maze width and height 
+		// If distance to the exit is 1 then increase number of exits
+		// Check if number of exits is 1
+		mazeFactory.order(stubOrder);
+		mazeFactory.waitTillDelivered();
+		mazeConfig = (MazeContainer)stubOrder.getMaze();
+		int numberOfExits = 0;
+		Distance distance = mazeConfig.getMazedists();
+		for (int i = 0; i < mazeConfig.getWidth(); i++) {
+			for (int j = 0; j < mazeConfig.getHeight(); j++) {
+				if (distance.getDistanceValue(i, j) == 1)
+					numberOfExits++;
+			}
+		}
+		assertEquals(1, numberOfExits);
+	}
+	
+	@Test
+	public void testMazeExitReachable() {
+		// Order maze from the MazeFactory
+		// Wait until the order is done
+		// Get maze from the StubOrder
+		// Get distance from exit object of maze
+		// Loop through with maze width and height 
+		// Check if distance to the exit is always greater than 0
+		mazeFactory.order(stubOrder);
+		mazeFactory.waitTillDelivered();
+		mazeConfig = (MazeContainer)stubOrder.getMaze();
+		Distance distance = mazeConfig.getMazedists();
+		for (int i = 0; i < mazeConfig.getWidth(); i++) {
+			for (int j = 0; j < mazeConfig.getHeight(); j++) {
+				assertTrue(distance.getDistanceValue(i, j) > 0);
+			}
+		}
+	}
+	
+	@Test
+	public void testPerfectMaze() {
+		// Order maze from the MazeFactory
+		// Wait until the order is done
+		// Get maze from the StubOrder
+		// Keep track if maze is perfect
+		// Get floorplan object of maze
+		// Loop through with maze width and height 
+		// If any spot has no walls then maze is perfect is false
+		// Check if maze is perfect is true
+		mazeFactory.order(stubOrder);
+		mazeFactory.waitTillDelivered();
+		mazeConfig = (MazeContainer)stubOrder.getMaze();
+		boolean isPerfect = true;
+		Floorplan floorplan = mazeConfig.getFloorplan();
+		for (int i = 0; i < mazeConfig.getWidth(); i++) {
+			for (int j = 0; j < mazeConfig.getHeight(); j++) {
+				if (floorplan.isInRoom(i, j))
+					isPerfect = false;
+				else
+					continue;
+			}
+		}
+		assertTrue(isPerfect);
+		mazeFactory.order(stubOrder2);
+		mazeFactory.waitTillDelivered();
+		mazeConfig2 = (MazeContainer)stubOrder2.getMaze();
+		boolean isPerfect2 = true;
+		Floorplan floorplan2 = mazeConfig2.getFloorplan();
+		for (int i = 0; i < mazeConfig2.getWidth(); i++) {
+			for (int j = 0; j < mazeConfig2.getHeight(); j++) {
+				if (floorplan2.isInRoom(i, j))
+					isPerfect2 = false;
+				else
+					continue;
+			}
+		}
+		assertFalse(isPerfect2);
+	}
+}
