44b89c79814ed7653acb7ce170834c05e88e6639
David Montenegro <dnmontenegro@email.wm.edu>
2021-06-19 04:07:04 -0400

    Upload of initial BasicRobot, BasicSensor, WallFollower, Wizard files

---
+++ MazeByDavidMontenegro/src/gui/BasicSensor.java
@@ -0,0 +1,61 @@
+package gui;
+
+import generation.CardinalDirection;
+import generation.Maze;
+import gui.Robot.Direction;
+/**
+ * Class: BasicSensor
+ * 
+ * Responsibilities:
+ * 
+ * Collaborators: DistanceSensor, MazeContainer, CardinalDirection
+ * 
+ * @author 2002d
+ *
+ */
+
+public class BasicSensor implements DistanceSensor {
+
+	public BasicSensor() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public int distanceToObstacle(int[] currentPosition, CardinalDirection currentDirection, float[] powersupply)
+			throws Exception {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void setMaze(Maze maze) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void setSensorDirection(Direction mountedDirection) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public float getEnergyConsumptionForSensing() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void startFailureAndRepairProcess(int meanTimeBetweenFailures, int meanTimeToRepair)
+			throws UnsupportedOperationException {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void stopFailureAndRepairProcess() throws UnsupportedOperationException {
+		// TODO Auto-generated method stub
+
+	}
+
+}
