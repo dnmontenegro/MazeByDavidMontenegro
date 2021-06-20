2592b086f1df2fd1243ed227f40e6f38e553e8e8
David Montenegro <dnmontenegro@email.wm.edu>
2021-06-20 08:23:57 -0400

    Completed Wizard Pseudocode

--- MazeByDavidMontenegro/src/gui/Wizard.java
+++ MazeByDavidMontenegro/src/gui/Wizard.java
@@ -1,6 +1,7 @@
 package gui;
 
 import generation.Maze;
+import generation.MazeContainer;
 /**
  * Class: Wizard
  * 
@@ -13,44 +14,80 @@ import generation.Maze;
  */
 
 public class Wizard implements RobotDriver {
+	
+	private BasicRobot basicRobot;
+	private MazeContainer maze;
 
+	/**
+	 * Constructor that setups initial objects.
+	 */
 	public Wizard() {
-		// TODO Auto-generated constructor stub
+		//Set basicRobot to null
+		//Set maze to null
 	}
 
+	/**
+	 * This method sets the robot to the parameter.
+	 */
 	@Override
 	public void setRobot(Robot r) {
-		// TODO Auto-generated method stub
-
+		//Set the robot to the parameter
 	}
 
+	/**
+	 * This method sets the maze to the parameter.
+	 */
 	@Override
 	public void setMaze(Maze maze) {
-		// TODO Auto-generated method stub
-
+		//Set the maze to the parameter
 	}
-
+	
+	/**
+	 * This method leads the robot to the exit using the strategy of using 
+	 * distances from the exit to find the closest path to the exit.
+	 */
 	@Override
 	public boolean drive2Exit() throws Exception {
-		// TODO Auto-generated method stub
+		//Loop until the robot is at the exit position
+			//Check if the robot has stopped
+				//If so throw exception
+			//Get the neighbor closest to the exit
+			//Move one step to the neighbor closest to the exit
+		//If at exit position rotate until robot is facing forward towards the exit and then return true
+		//Otherwise return false
 		return false;
 	}
-
+	
+	/**
+	 * This method leads the robot one step towards the exit using the strategy of using 
+	 * distances from the exit to find the closest path to the exit.
+	 */
 	@Override
 	public boolean drive1Step2Exit() throws Exception {
-		// TODO Auto-generated method stub
+		//Check if the robot has stopped
+			//If so throw exception
+		//Get the neighbor closest to the exit
+		//Move one step to the neighbor closest to the exit and return true
+		//If at exit position rotate until robot is facing forward towards the exit and then return false
+		//Otherwise return false
 		return false;
 	}
 
+	/**
+	 * This method gets the amount of energy used during traversal of the maze.
+	 */
 	@Override
 	public float getEnergyConsumption() {
-		// TODO Auto-generated method stub
+		//Return 2000 subtracted by the battery level of the robot
 		return 0;
 	}
 
+	/**
+	 * This method gets the amount of cells traveled during traversal of the maze.
+	 */
 	@Override
 	public int getPathLength() {
-		// TODO Auto-generated method stub
+		//Return the odometer reading of the robot
 		return 0;
 	}
 
